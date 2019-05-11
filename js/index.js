c701c62891113ec52058dfdb4e46bc1023d12619
Emilee537 <emileejlindner@gmail.com>
2019-05-11 19:42:34 -0400

    updated if else statements

--- js/index.js
+++ js/index.js
@@ -10,6 +10,22 @@ $(function() {
       console.log("clicked");
       let city = $("#city-type").val();
       console.log(city);
+
+      if (city === "new york city" || "new york" || "nyc") {
+        $("body").attr('class', 'nyc');
+      } else if (city === "San Francisco" || "SF" || "Bay Area") {
+        $("body").attr('class', 'sf');
+      } else if (city === "Los Angeles" || "LA" || "LAX") {
+        $("body").attr('class', 'la');
+      } else if (city === "Austin" || "ATX") {
+        $("body").attr('class', 'austin');
+      } else if (city === "Sydney" || "SYD") {
+        $("body").attr('class', 'sydney');
+      } else {
+
+      }
+
+
     }
 
 });
